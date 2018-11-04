dcc62b1c0650a1cdee522c3cf68c06a1840b4bb9
Rivo <Rivo@DESKTOP-91Q61VH>
2018-11-04 14:23:07 +0700

    menyelesaikan class main untuk running

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  * @author Rivo
  */
 public class Main {
-    
+    public static void main(String[] args){
+        Service S = new Service("Cuci Wax", 100000);
+        Mobil M = new Mobil("Small", 100000, "Brio");
+        Pegawai P = new Pegawai("Robert", "Pencuci");
+        Transaksi T = new Transaksi(M, S, P);
+        
+        T.print();
+    }
 }
