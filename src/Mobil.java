095b1186a58cb3bf62a48d838e850e4a6567a05d
Rivo <Rivo@DESKTOP-91Q61VH>
2018-11-04 14:04:38 +0700

    menyelesaikan class mobil untuk memberikan data mobil apa yang di beri layanan

--- src/Mobil.java
+++ src/Mobil.java
@@ -1,13 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  * @author Rivo
  */
 public class Mobil {
+    private String jenis;
+    private int harga;
+    private String merk;
+
+    Mobil(String jenis, int harga, String merk) {
+        this.jenis = jenis;
+        this.harga = harga;
+        this.merk = merk;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
     
+    public String info(){
+        String info = "";
+        info +="Jenis Mobil : " + this.jenis + "\n";
+        info +="Merk Mobil : " + this.merk + "\n";
+        info +="Harga : " + this.harga + "\n";
+        return info;
+    }
 }
