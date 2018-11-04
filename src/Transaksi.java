02a40340b653b1b941743a694359cdc10a809885
Rivo <Rivo@DESKTOP-91Q61VH>
2018-11-04 13:37:40 +0700

    membuat class class, dan mengisi atribut konstruktor pada class transaksi

---
+++ src/Transaksi.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rivo
+ */
+public class Transaksi {
+    private Mobil mobil;
+    private Service service;
+    private Pegawai pegawai;
+
+    public Transaksi(Mobil mobil, Service service, Pegawai pegawai) {
+        this.mobil = mobil;
+        this.service = service;
+        this.pegawai = pegawai;
+    }
+    
+    
+}
