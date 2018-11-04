5c468a55324ce4cc0f3143d062b320baeb723d24
Rivo <Rivo@DESKTOP-91Q61VH>
2018-11-04 14:11:40 +0700

    menyelesaikan class pegawai dimana class ini untuk menginfokan siapa pegawai beserta pekerjaanya untuk tiap layanan yang diberikan

--- src/Pegawai.java
+++ src/Pegawai.java
@@ -1,13 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
  * @author Rivo
  */
 public class Pegawai {
+    private String nama;
+    private String pekerjaan;
+
+    Pegawai(String nama, String pekerjaan) {
+        this.nama = nama;
+        this.pekerjaan = pekerjaan;
+    }
+    
+    public String info(){
+        String info = "";
+        info +="Nama : " + this.nama + "\n";
+        info +="Pekerjaan : " + this.pekerjaan + "\n";
+        return info;
+    }
+    
     
 }
